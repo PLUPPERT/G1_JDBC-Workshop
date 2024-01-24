f3c54f21df74a63ef7c24cbf4d8e9f95267b8b7a
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2024-01-24 18:50:55 +0100

    - Small correction in ShoppingCartDAO.java

--- src/main/java/org/lexicon/data/ShoppingCartDAO.java
+++ src/main/java/org/lexicon/data/ShoppingCartDAO.java
@@ -6,14 +6,10 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ShoppingCartDAO {
-<<<<<<< HEAD
-
-=======
     ShoppingCartDAO INSTANCE = ShoppingCartDAORepository.getInstance();
     ShoppingCart save(ShoppingCart cart);
     Optional<ShoppingCart> findById(int id);
     List<ShoppingCart> findAll();
     List<ShoppingCart> findByReference(String customer);
     void delete(int id);
->>>>>>> f8efda47983495f3a506c7be099f977c37af1f73
 }
